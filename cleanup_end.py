3a7dd3082fa29770cb15870ef255579df8823a84
2025sunhua-sketch <2025sunhua@gmail.com>
2026-09-15 23:08:59 +0800

    chore: remove last cleanup script

--- cleanup_end.py
+++ cleanup_end.py
@@ -4,10 +4,10 @@ workspace = r"C:\Users\ADMIN\.jvs\.openclaw\workspace\kunde-website"
 os.chdir(workspace)
 
 # 删除自身
-script_path = os.path.join(workspace, "final_clean.py")
+script_path = os.path.join(workspace, "done_cleanup.py")
 if os.path.exists(script_path):
     os.remove(script_path)
-    print("Deleted: final_clean.py")
+    print("Deleted: done_cleanup.py")
 
 # Git add -A 并提交
 subprocess.run(["git", "add", "-A"], check=True)
